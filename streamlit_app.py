352a45d27bbfe74d59c3c763b091f3baa4555c7f
Markus Fuzari <markusfuzari@gmail.com>
2024-08-02 19:58:28 -0300

    Ajustar app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,52 @@
 import streamlit as st
+import pickle
 import pandas as pd
-import math
-from pathlib import Path
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
+# Carregar o modelo
+with open('./data/modelo_paciente_hipertensao.pkl', 'rb') as file:
+    modelo = pickle.load(file)
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
+# Função para fazer predições
+def fazer_predicao(dados):
+    predicoes = modelo.predict(dados)
+    return predicoes
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+# Interface do usuário no Streamlit
+st.title('Previsão de Hipertensão')
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
+# Campos de entrada para o usuário
+idade = st.number_input('Idade')
+sexo = st.selectbox('Sexo', ['Masculino', 'Feminino'])
+tipo_dor = st.selectbox('Tipo de Dor no Peito', [1, 2, 3, 4])
+pressao_sanguinea = st.number_input('Pressão Sanguínea em Repouso')
+colesterol = st.number_input('Colesterol')
+nivel_acucar = st.selectbox('Nível de Açúcar no Sangue em Jejum', [0, 1])
+ecg = st.selectbox('Resultados do ECG em Repouso', [0, 1, 2])
+max_freq_cardiaca = st.number_input('Frequência Cardíaca Máxima Alcançada')
+angina_exercicio = st.selectbox('Angina Induzida pelo Exercício', [0, 1])
+depressao_st = st.number_input('Depressão do Segmento ST Induzida por Exercício')
+slope_st = st.selectbox('Inclinação do Segmento ST Pico de Exercício', [1, 2, 3])
+vasos = st.number_input('Número de Vasos Coloridos por Fluoroscopia')
+defeito = st.selectbox('Defeito Reversível ou Fixo', [3, 6, 7])
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+# Criar um DataFrame com os dados inseridos
+dados_usuario = pd.DataFrame({
+    'idade': [idade],
+    'sexo': [1 if sexo == 'Masculino' else 0],
+    'tipo_de_dor': [tipo_dor],
+    'pressao_sanguinea': [pressao_sanguinea],
+    'colesterol': [colesterol],
+    'nivel_acucar': [nivel_acucar],
+    'ecg': [ecg],
+    'max_freq_cardiaca': [max_freq_cardiaca],
+    'angina_exercicio': [angina_exercicio],
+    'depressao_st': [depressao_st],
+    'slope_st': [slope_st],
+    'vasos': [vasos],
+    'defeito': [defeito]
+})
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
-
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
-
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
-
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
-
-
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
-
-st.header(f'GDP in {to_year}', divider='gray')
-
-''
-
-cols = st.columns(4)
-
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+# Fazer predição
+if st.button('Fazer Previsão'):
+    resultado = fazer_predicao(dados_usuario)
+    st.write(f'Predição: {resultado[0]}')
