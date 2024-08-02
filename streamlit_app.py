8bf558451a29e37c9e9e191e1108ccb5e1bd56a5
Markus Fuzari <markusfuzari@gmail.com>
2024-08-02 20:02:42 -0300

    Adicionar tratativa visual

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,52 +1,62 @@
 import streamlit as st
-import pickle
 import pandas as pd
+import plotly.express as px
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-# Carregar o modelo
-with open('./data/modelo_paciente_hipertensao.pkl', 'rb') as file:
-    modelo = pickle.load(file)
+# Carrega o arquivo CSV
+df = pd.read_csv('../Resultado.csv')
 
-# Função para fazer predições
-def fazer_predicao(dados):
-    predicoes = modelo.predict(dados)
-    return predicoes
+# Título do app
+st.title('Visualização de Dados de Hipertensão')
 
-# Interface do usuário no Streamlit
-st.title('Previsão de Hipertensão')
+# Filtro por sexo
+sexo = st.sidebar.selectbox('Filtrar por Sexo', ['Todos', 'Feminino', 'Masculino'])
 
-# Campos de entrada para o usuário
-idade = st.number_input('Idade')
-sexo = st.selectbox('Sexo', ['Masculino', 'Feminino'])
-tipo_dor = st.selectbox('Tipo de Dor no Peito', [1, 2, 3, 4])
-pressao_sanguinea = st.number_input('Pressão Sanguínea em Repouso')
-colesterol = st.number_input('Colesterol')
-nivel_acucar = st.selectbox('Nível de Açúcar no Sangue em Jejum', [0, 1])
-ecg = st.selectbox('Resultados do ECG em Repouso', [0, 1, 2])
-max_freq_cardiaca = st.number_input('Frequência Cardíaca Máxima Alcançada')
-angina_exercicio = st.selectbox('Angina Induzida pelo Exercício', [0, 1])
-depressao_st = st.number_input('Depressão do Segmento ST Induzida por Exercício')
-slope_st = st.selectbox('Inclinação do Segmento ST Pico de Exercício', [1, 2, 3])
-vasos = st.number_input('Número de Vasos Coloridos por Fluoroscopia')
-defeito = st.selectbox('Defeito Reversível ou Fixo', [3, 6, 7])
+if sexo == 'Feminino':
+    df = df[df['sexo'] == 0]
+elif sexo == 'Masculino':
+    df = df[df['sexo'] == 1]
 
-# Criar um DataFrame com os dados inseridos
-dados_usuario = pd.DataFrame({
-    'idade': [idade],
-    'sexo': [1 if sexo == 'Masculino' else 0],
-    'tipo_de_dor': [tipo_dor],
-    'pressao_sanguinea': [pressao_sanguinea],
-    'colesterol': [colesterol],
-    'nivel_acucar': [nivel_acucar],
-    'ecg': [ecg],
-    'max_freq_cardiaca': [max_freq_cardiaca],
-    'angina_exercicio': [angina_exercicio],
-    'depressao_st': [depressao_st],
-    'slope_st': [slope_st],
-    'vasos': [vasos],
-    'defeito': [defeito]
-})
+# Filtro por faixa etária
+idade_min, idade_max = st.sidebar.slider(
+    'Selecione a faixa etária',
+    min_value=int(df['idade'].min()),
+    max_value=int(df['idade'].max()),
+    value=(int(df['idade'].min()), int(df['idade'].max()))
+)
 
-# Fazer predição
-if st.button('Fazer Previsão'):
-    resultado = fazer_predicao(dados_usuario)
-    st.write(f'Predição: {resultado[0]}')
+df = df[(df['idade'] >= idade_min) & (df['idade'] <= idade_max)]
+
+
+# Gráfico de Pizza: Quantidade Total de Hipertensão
+st.header('Gráfico Quantidade Total de Hipertensão')
+hipertensao_counts = df['previsao'].value_counts().reset_index()
+hipertensao_counts.columns = ['previsao', 'count']
+hipertensao_counts['previsao'] = hipertensao_counts['previsao'].map({0: 'Sem Hipertensão', 1: 'Com Hipertensão'})
+fig_pizza = px.pie(hipertensao_counts, values='count', names='previsao', 
+                   title='Quantidade Total de Pessoas com ou sem Hipertensão')
+st.plotly_chart(fig_pizza)
+
+
+# Gráfico de Linha: Quantidade total de Hipertensão ou não por Idade
+st.header('Quantidade total de Hipertensão ou não por Idade')
+df_grouped = df.groupby(['idade', 'previsao']).size().reset_index(name='count')
+fig_linha_hipertensao = px.line(df_grouped, x='idade', y='count', color='previsao',
+                               labels={'idade': 'Idade', 'count': 'Quantidade', 'previsao': 'Hipertensão'},
+                               title='Quantidade total de Hipertensão ou não por Idade')
+fig_linha_hipertensao.update_traces(mode='lines+markers')
+st.plotly_chart(fig_linha_hipertensao)
+
+# Gráfico de Dispersão: Frequência Cardíaca Máxima por Idade
+st.header('Frequência Cardíaca Máxima por Idade')
+fig_dispercao_frequencia = px.scatter(df, x='idade', y='maior_frequencia_cardiaca',
+                                      labels={'idade': 'Idade', 'maior_frequencia_cardiaca': 'Frequência Cardíaca Máxima'},
+                                      title='Frequência Cardíaca Máxima por Idade')
+fig_dispercao_frequencia.update_traces(marker=dict(size=10, color='rgba(219, 64, 82, 0.8)', line=dict(width=2, color='rgba(219, 64, 82, 0.8)')))
+st.plotly_chart(fig_dispercao_frequencia)
+
+
+# Exibe o DataFrame
+st.write("Dados do Arquivo CSV")
+st.dataframe(df)
